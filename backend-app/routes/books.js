5c47b89b8468bc856987ca839573c8fdf0c01a78
Satyam Singh <satyamsingh1991@gmail.com>
2024-04-23 22:27:02 +0530

    Update books.js
    conflict removal

--- backend-app/routes/books.js
+++ backend-app/routes/books.js
@@ -1,32 +1,19 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
-const Book = require('../models/book');
-=======
+
 const Book = require('../models/books');
->>>>>>> dc1facd (changes)
 
 router.get('/books', async (req, res) => {
   try {
     const { title, author, genre, sortOrder, startDate, endDate, page, pageSize } = req.query;
-<<<<<<< HEAD
-
-    const query = {};
-    if (title) query.title = new RegExp(title, 'i'); // Case-insensitive partial match
-=======
     const query = {};
     if (title) query.title = new RegExp(title, 'i'); 
->>>>>>> dc1facd (changes)
     if (author) query.author = new RegExp(author, 'i');
     if (genre) query.genre = new RegExp(genre, 'i');
     if (startDate && endDate) {
       query.published_date = { $gte: new Date(startDate), $lte: new Date(endDate) };
     }
 
-<<<<<<< HEAD
-=======
-  
->>>>>>> dc1facd (changes)
     const books = await Book.find(query)
       .sort(sortOrder)
       .skip((page - 1) * pageSize)
@@ -39,20 +26,15 @@ router.get('/books', async (req, res) => {
   }
 });
 
-<<<<<<< HEAD
 
-router.post('/books', async (req, res) => {
-  try {
-    // Extract book details from request body
-    const { title, author, published_date, genre, description, ISBN, availability } = req.body;
-
-    // Create a new book object
-=======
 router.post('/books', async (req, res) => {
   try {
     const { title, author, published_date, genre, description, ISBN, availability } = req.body;
 
->>>>>>> dc1facd (changes)
+router.post('/books', async (req, res) => {
+  try {
+    const { title, author, published_date, genre, description, ISBN, availability } = req.body;
+
     const newBook = new Book({
       title,
       author,
@@ -62,11 +44,7 @@ router.post('/books', async (req, res) => {
       ISBN,
       availability
     });
-
-<<<<<<< HEAD
-   
-=======
->>>>>>> dc1facd (changes)
+    
     const savedBook = await newBook.save();
 
     res.status(201).json(savedBook);
@@ -76,8 +54,5 @@ router.post('/books', async (req, res) => {
   }
 });
 
-<<<<<<< HEAD
+
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> dc1facd (changes)
