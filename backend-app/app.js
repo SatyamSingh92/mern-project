665e97bc0aff736c32f146c1c1dcc8cb050dbc56
Satyam Singh <satyamsingh1991@gmail.com>
2024-04-23 22:25:10 +0530

    Update app.js
    removing conflicts

--- backend-app/app.js
+++ backend-app/app.js
@@ -6,17 +6,10 @@ const bookRoutes = require('./routes/books');
 
 const app = express();
 
-<<<<<<< HEAD
-app.use(cors()); 
-app.use(bodyParser.json()); 
-
-=======
-
 app.use(cors()); 
 app.use(bodyParser.json()); 
 
 
->>>>>>> dc1facd (changes)
 mongoose.connect('mongodb://localhost:27017/bookSchema', {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -26,25 +19,15 @@ mongoose.connect('mongodb://localhost:27017/bookSchema', {
   console.error('Error connecting to MongoDB:', error);
 });
 
-<<<<<<< HEAD
-// Routes
-app.use('/api', bookRoutes);
-
-=======
 
 app.use('/api', bookRoutes);
->>>>>>> dc1facd (changes)
+
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
 });
 
-<<<<<<< HEAD
-// Start the server
-=======
-
->>>>>>> dc1facd (changes)
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
